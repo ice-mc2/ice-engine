34eb1477beb71e70aabfd8be04972f211e54247b
Jun Zheng <me@jackzh.com>
2020-01-19 22:42:53 -0500

    chore: Remove config and server controllers.

--- app.js
+++ app.js
@@ -4,15 +4,6 @@ var http       = require('http');					// http module
 var bodyParser = require('body-parser');	// body-parser module for reading request body
 var server     = http.createServer(app);		// creating server
 
-const Controllers = {
-    SystemInfoController: require('./controllers/rest/SystemInfoController'),
-    ImageLibraryController: require('./controllers/rest/ImageLibraryController')
-};
-
-app.get('/api/v1', Controllers.SystemInfoController.getBasicSystemInformation);
-app.get('/api/v1/image-libraries', Controllers.ImageLibraryController.getAllAccredited);
-
-
 var port = process.env.PORT || 8080;
 server.listen(port);		// server starting on port '8080'
 
--- controllers/rest/ImageLibraryController.js
+++
@@ -1,7 +0,0 @@
-const config = require('../../config/config');
-
-module.exports = {
-    getAllAccredited: function(req, res){
-        res.send(config.accreditedImageLibraries);
-    }
-}
